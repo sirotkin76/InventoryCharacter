d20e830dc3d01cac08272487146f2e106abfb785
72gsir <72gsir@gmail.com>
2018-01-26 15:48:24 +0300

    Add game

---
+++ Assets/Scripts/InfoKeyManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoKeyManager : MonoBehaviour {
+
+	void FixedUpdate () {
+		if (Input.GetKeyDown(KeyCode.Escape)) 
+			Application.Quit();
+	}
+
+}
